a070a9e5acfdb64fc30c9fa94b7dfab78209c32b
Elijah M. Immer <me@eimmer.me>
2025-11-18 13:52:31 -0800

    Make matrix file pretty

--- matrix.h
+++ matrix.h
@@ -1,9 +1,9 @@
 /*
- *  Matrix header
- *  Function prototypes, data, and constants for matrix module
+ * Matrix header
+ * Function prototypes, data, and constants for matrix module
  *
- *  University of Washington, Tacoma
- *  TCSS 422 - Operating Systems
+ * University of Washington, Tacoma
+ * TCSS 422 - Operating Systems
  */
 
 #define ROW 5
@@ -12,18 +12,18 @@
 typedef struct matrix {
   int rows;
   int cols;
-  int ** m;
+  int **m;
 } Matrix;
 
 //extern int theseed;
 
 // MATRIX ROUTINES
-Matrix * AllocMatrix(int r, int c);
-void FreeMatrix(Matrix * mat);
-void GenMatrix(Matrix * mat);
-Matrix * GenMatrixRandom();
-int AvgElement(Matrix * mat);
-int SumMatrix(Matrix * mat);
-Matrix * MatrixMultiply(Matrix * m1, Matrix * m2);
-void DisplayMatrix(Matrix * mat, FILE *stream);
-Matrix * GenMatrixBySize(int row, int col);
+Matrix *AllocMatrix(int r, int c);
+void FreeMatrix(Matrix *mat);
+void GenMatrix(Matrix *mat);
+Matrix *GenMatrixRandom();
+int AvgElement(Matrix *mat);
+int SumMatrix(Matrix *mat);
+Matrix *MatrixMultiply(Matrix *m1, Matrix *m2);
+void DisplayMatrix(Matrix *mat, FILE *stream);
+Matrix *GenMatrixBySize(int row, int col);
