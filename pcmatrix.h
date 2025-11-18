fd625d015f22d7599238db2ba34cb87ce82b023f
Elijah M. Immer <me@eimmer.me>
2025-11-18 10:24:20 -0800

    Comment all asserts

--- pcmatrix.h
+++ pcmatrix.h
@@ -14,14 +14,17 @@
 
 // Size of the buffer ARRAY  (see ch. 30, section 2, producer/consumer)
 #define MAX 200
+/// should be a `size_t` and defaultly set to `MAX`
 int BOUNDED_BUFFER_SIZE;
 
 // Number of matrices to produce/consume
 #define LOOPS 1200
+/// should be a `size_t` and defaultly set to `LOOPS`
 int NUMBER_OF_MATRICES;
 
 // MATRIX MODE FLAG
 // mode 0 - Generate random matricies
 // mode 1-n - Specifies a fixed number of rows and cols with matrix elements of 1
 #define DEFAULT_MATRIX_MODE 0
+/// Should be a `size_t` and set to `DEFAULT_MATRIX_MODE`
 int MATRIX_MODE;
--- prodcons.c
+++ prodcons.c
@@ -35,68 +35,65 @@ size_t bounded_buffer_readable = 0;
 
 // Bounded buffer put() get()
 /// Thread safe
+/// Whomever `get`s the value owns it, do not free it until then.
 int put(Matrix * value) {
   assert(value != NULL);
   pthread_mutex_lock(&bounded_buffer_mutex);
-  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE);
-  assert(bounded_buffer_readable >= 0);
-  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1);
-  assert(bounded_buffer_write_idx >= 0);
+  assert(BOUNDED_BUFFER_SIZE > 0 && "Buffer must be a valid size");
+  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE && "cannot have more readable than slots exists");
+  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "write_idx must be within the buffer");
 
   while (bounded_buffer_readable == (size_t) BOUNDED_BUFFER_SIZE) {
+    assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1  && "write_idx must be within the buffer");
     // no space to write, wait until a space opens up.
     pthread_cond_wait(&bounded_buffer_put_cond, &bounded_buffer_mutex);
   }
-  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE);
-  assert(bounded_buffer_readable >= 0);
-  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1);
-  assert(bounded_buffer_write_idx >= 0);
+  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE && "cannot have more readable than slots exist");
+  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "must have an extra slot open for the read");
 
-  assert(bigmatrix[bounded_buffer_write_idx] == NULL);
+  assert(bigmatrix[bounded_buffer_write_idx] == NULL && "overridden entry must have been cleared");
   bigmatrix[bounded_buffer_write_idx] = value;
 
   bounded_buffer_write_idx = (bounded_buffer_write_idx + 1) % (size_t) BOUNDED_BUFFER_SIZE;
-  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1);
-  assert(bounded_buffer_write_idx >= 0);
+  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "new index must be within buffer");
 
   bounded_buffer_readable += 1;
-  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE);
-  assert(bounded_buffer_readable >= 1);
+  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE && "cannot have more readable than space available");
 
   pthread_cond_signal(&bounded_buffer_get_cond);
   pthread_mutex_unlock(&bounded_buffer_mutex);
   return 0;
 }
 
+/// Thread safe
+/// Caller takes ownership of return, and it just be freed.
 Matrix * get() {
   pthread_mutex_lock(&bounded_buffer_mutex);
-  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE);
-  assert(bounded_buffer_readable >= 0);
-  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1);
-  assert(bounded_buffer_write_idx >= 0);
+  assert(BOUNDED_BUFFER_SIZE > 0 && "Buffer must be a valid size");
+  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE && "cannot have more readable than slots exists");
+  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "write_idx must be within the buffer");
 
   while (bounded_buffer_readable <= 0) {
     // nothing to read, wait until a slot fills.
     pthread_cond_wait(&bounded_buffer_get_cond, &bounded_buffer_mutex);
   }
-  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE);
-  assert(bounded_buffer_readable >= 1);
+  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE && "cannot have more readable than slots exist");
+  assert(bounded_buffer_readable >= 1 && "must have at least 1 slot to read");
+  assert(bounded_buffer_write_idx <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "must have an extra slot open for the read");
 
   // TODO(Elijah): Does this math, maths?
   size_t idx = (bounded_buffer_write_idx >= bounded_buffer_readable)
     ? bounded_buffer_write_idx - bounded_buffer_readable
     : (size_t) BOUNDED_BUFFER_SIZE - (bounded_buffer_readable - bounded_buffer_write_idx);
 
-  assert(idx <= (size_t) BOUNDED_BUFFER_SIZE - 1);
-  assert(idx >= 0);
+  assert(idx <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "retreival index just be be within the buffer");
 
-  assert(bigmatrix[idx] != NULL);
+  assert(bigmatrix[idx] != NULL && "Entry read be filled (i.e. not NULL)");
   Matrix *value = bigmatrix[idx];
-  bigmatrix[idx] = NULL;
+  bigmatrix[idx] = NULL; // clear position we just took
 
   bounded_buffer_readable -= 1;
-  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE - 1);
-  assert(bounded_buffer_readable >= 0);
+  assert(bounded_buffer_readable <= (size_t) BOUNDED_BUFFER_SIZE - 1 && "must have less entries than buffer size (-1 since we just took one)");
 
   pthread_cond_signal(&bounded_buffer_put_cond);
   pthread_mutex_unlock(&bounded_buffer_mutex);
